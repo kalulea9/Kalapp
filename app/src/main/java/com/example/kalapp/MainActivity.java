7be9e4754edfe42451259dce946b85da5db9a2ff
kalulea9 <kalulea9@gmail.com>
2020-02-19 16:42:46 +0300

    hello

--- app/src/main/java/com/example/kalapp/MainActivity.java
+++ app/src/main/java/com/example/kalapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+   public void sendMessage(View view){
+        EditText message=(EditText) findViewById(R.id.message);
+       Toast.makeText(this,"sending message"+message.getText().toString(),
+               Toast.LENGTH_SHORT).show();
+       Intent intent = new Intent(this, DisplayMessageActivity.class);
+       intent.putExtra("MESSAGE", message.getText().toString()); startActivity(intent);
+       message.setText(""); }
 }
+
